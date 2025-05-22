f500bf995066daf189a11e4f8453d2bbe129a399
vamshi <mvamshi952@gmail.com>
2025-05-22 16:16:39 +0530

    first commit

---
+++ src/main/java/com/alephys/AlphysApplication.java
@@ -0,0 +1,13 @@
+package com.alephys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphysApplication.class, args);
+	}
+
+}
